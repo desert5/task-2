f9b6404e2b65098bca1a0f5c2ce481c9d838237e
desert5 <nova5@email.com>
2020-03-09 04:41:54 +0200

    Added redis

--- rating-calculator/src/main/java/task2/rating/calculator/RatingCalculatorApplication.java
+++ rating-calculator/src/main/java/task2/rating/calculator/RatingCalculatorApplication.java
@@ -1,13 +1,34 @@
 package task2.rating.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RatingCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatingCalculatorApplication.class, args);
+	}
+
+	@Bean
+	@Autowired
+	public LettuceConnectionFactory lettuceConnectionFactory(@Value("${redis.host}") String redisHost) {
+		return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost));
+	}
+
+
+	@Bean
+	@Autowired
+	public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(connectionFactory);
+		return redisTemplate;
 	}
 
 }
--- rating-calculator/src/main/java/task2/rating/calculator/service/RatingCalculationService.java
+++ rating-calculator/src/main/java/task2/rating/calculator/service/RatingCalculationService.java
@@ -2,18 +2,25 @@ package task2.rating.calculator.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import task2.common.Constants;
-import task2.common.model.KafkaMessage;
 
 @Service
 public class RatingCalculationService {
     private final Logger logger = LoggerFactory.getLogger(RatingCalculationService.class);
-    private KafkaMessage message;
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    public RatingCalculationService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     @KafkaListener(topics = Constants.KAFKA_TOPIC, groupId = "group_id")
     public void consumeMessage(String message) {
         logger.info(String.format("#### -> Consumed message -> %s", message));
+        redisTemplate.opsForValue().set("key", message);
     }
 }
